858508d01e93128763203b70a160c650ac6c44ec
coinfxpro <154015369+coinfxpro@users.noreply.github.com>
2025-02-24 16:44:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -700,4 +700,7 @@ def daily_transactions():
         return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    # Worker'ı başlatma komutu
+    os.system('python worker.py &')
     app.run(debug=True, host='0.0.0.0', port=5001)
+
